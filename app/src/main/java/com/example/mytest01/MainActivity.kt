c8972e0a8648bdb7554a552d8c04222057259d2f
Jin <songtaejin990@gmail.com>
2023-11-25 17:16:53 +0900

    update MainActivity

--- app/src/main/java/com/example/mytest01/MainActivity.kt
+++ app/src/main/java/com/example/mytest01/MainActivity.kt
@@ -2,20 +2,19 @@ package com.example.mytest01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mytest01.adapters.PersonAdapter
 import com.example.mytest01.databinding.ActivityMainBinding
 import com.example.mytest01.datas.PersonInfo
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var pDatas = mutableListOf<PersonInfo>(
+        val pDatas = mutableListOf(
             PersonInfo("John1", "i am jokdns1 ksf skfd skkw sfds", R.mipmap.ic_launcher),
             PersonInfo("John2", "i am jokdns2 ksf skfd skkw sfds", R.mipmap.ic_launcher),
             PersonInfo("John3", "i am jokdns3 ksf skfd skkw sfds", R.mipmap.ic_launcher),
